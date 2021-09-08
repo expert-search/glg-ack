e2ab3fef0310e61abd2d8fdf03ae4a952d0adcf7
Andrew Minh Nguyen <amqdnguyen@gmail.com>
2021-09-08 02:45:02 +0000

    Add quick entity filtering for experiments

--- src/deployment/app.py
+++ src/deployment/app.py
@@ -1,11 +1,12 @@
 import pickle
 from pathlib import Path
 
+import spacy
 import tensorflow as tf
 import streamlit as st
 import pandas as pd
 import altair as alt
-from transformers.models.bert.modeling_tf_bert import TFBertMainLayer
+from transformers.models.bert.modeling_tf_bert import TFBertMainLayer  # required
 
 
 ## Page Setup
@@ -19,14 +20,12 @@ with st.sidebar:
 ## Session State
 model_dir = Path('__file__').resolve().parents[2]/'models'
 def get_object(fname, fmethod='rb'):
-    if '.hdf5' in fname:
-        return model_dir/fname
     with open(model_dir/fname, fmethod) as f:
         return pickle.load(f) if '.pkl' in fname else f.read()
 
 def get_bert_clf():
     model = tf.keras.models.model_from_json(get_object('bert_model.json', 'r'))
-    model.load_weights(get_object('bert_weights.hdf5'))
+    model.load_weights(model_dir/'bert_weights.hdf5')
     return model
 
 session_vars = {'tfidf', 'clf', 'bert_clf', 'bert_tkzr'}
@@ -35,11 +34,15 @@ if not session_vars.intersection(set(st.session_state)):
     st.session_state.clf = get_object('grail_qa_lr.pkl')
     st.session_state.bert_tkzr = get_object('bert_tokenizer.pkl')
     st.session_state.bert_clf = get_bert_clf()
+    st.session_state.ner = spacy.load(model_dir/'kaggleTrainedNER10000')  # spacy.load('en_core_web_sm')  
 
 tfidf = st.session_state.tfidf
 clf = st.session_state.clf
 bert_tkzr = st.session_state.bert_tkzr
 bert_clf = st.session_state.bert_clf
+ner = st.session_state.ner
+
+## Helpers
 
 def predict(query, clf_type):
     clf_types = ['bert', 'lr']
@@ -48,11 +51,18 @@ def predict(query, clf_type):
     if clf_type == 'bert':
         x = bert_tkzr(query, padding='max_length', max_length=100, truncation=True, return_tensors='tf')
         pred = bert_clf(x['input_ids'])
-        pred = tf.math.softmax(pred['label']).numpy()  # Convert to probabilities
+        pred = tf.math.sigmoid(pred['label']).numpy()  # Convert to probabilities
     if clf_type == 'lr':
         pred = clf.predict_proba(tfidf.transform([query]))
     
     return pred
+
+def apply_ner(query, ner):
+    pred = ner(query)
+    return [ent.text for ent in pred.ents]
+
+def show_df(labels):
+    st.write(df[df['Label']].reset_index(drop=True))
 
 ## Apps
 if page == SIMPLE_PREDICTION:
@@ -61,36 +71,42 @@ if page == SIMPLE_PREDICTION:
 
     Our model automatically classifies client queries as being either healthcare-related or technology-related. You can test it out below.
     """
-    query = st.text_input('Enter your query:')
+    query = st.text_area('Enter your query:')
     if len(query) == 0:
-        st.write('_Prediction: _')
+        st.write('Prediction:')
+        st.write('Entities:')
     else:
         pred = predict(query, 'bert')
         pred = pd.DataFrame(pred, index=['probability'], columns=['Healthcare', 'Technology'])
         st.write(pred.style.format(precision=4))
         healthcare_proba, technology_proba = pred.Healthcare[0], pred.Technology[0]
-        if healthcare_proba < 0.7 and technology_proba < 0.7:
+        if healthcare_proba < 0.8 and technology_proba < 0.8:
             pred = 'Other'
         else:
             pred = 'Healthcare' if healthcare_proba > technology_proba else 'Technology'
-        st.write(f'_Prediction: {pred}_')
+        st.write(f'Prediction: {pred}')
+        st.write(f'Entities: {apply_ner(query, ner)}')
 
 elif page == SHOWCASE:
     # MORE FEATURES:
     # - Provide DF with ['date_received', 'client', 'label', 'hover_for_full_query', 'NER hashtags'] -- clients can be random from Fortune-500
     # - Hashtags show a DF with connected experts (random names)
-    # - Provide manual review
     # - Show graph of entity tags over time
-    from demo import n_tech, n_heal, df
+    from demo import n_tech, n_heal, n_else, df_chart, df, entities
     """
     # Welcome, GLG Agent
     ---
     """
-    c = alt.Chart(df, title='Last Week\'s Queries Per Day').mark_line().encode(x=alt.X('monthdate(Date)', title='Date'), y=alt.Y('count(Client)', title='Num Queries'), color='Label').configure_legend(title=None, orient='bottom-left')
+    c = alt.Chart(df_chart, title='Queries Over the Past Week').mark_line().encode(x=alt.X('monthdate(Date)', title='Day'), y=alt.Y('count(Client)', title='Num Queries'), color='Label').configure_legend(title=None, orient='bottom-left')
     st.altair_chart(c, use_container_width=True)
-    f"## There are {n_tech} technology queries available:"
+    cols = ['Client', 'Label', 'Date']
+    f"## There are {n_tech} technology queries available today:"
     with st.expander('View'):
-        st.write(df[df['Label'] == 'Technology'].reset_index(drop=True))
-    f"## There are {n_heal} healthcare queries available:"
+        tags = st.multiselect('', entities)
+        st.write(df.loc[(df['Label'] == 'Technology') & (df.iloc[:, 3:].isin(tags).any(axis=1)), cols].reset_index(drop=True))
+    f"## There are {n_heal} healthcare queries available today:"
     with st.expander('View'):
         st.write(df[df['Label'] == 'Healthcare'].reset_index(drop=True))
+    f"## There are {n_else} queries needing manual review today:"
+    with st.expander('View'):
+        st.write(df[df['Label'] == 'Other'].reset_index(drop=True))
--- src/deployment/demo.py
+++ src/deployment/demo.py
@@ -5,11 +5,13 @@ import numpy as np
 import pandas as pd
 
 today = date.today()
-past_week = [today - timedelta(days=i) for i in range(8)]
+past_week = [today - timedelta(days=i) for i in range(1, 8)]
 
 # Randomize sample size
 n_tech = np.random.randint(150, 196)
 n_heal = np.random.randint(70, 79)
+n_done = np.random.randint(1300, 1700)
+n_else = np.random.randint(12, 18)
 
 df_tech = pd.read_csv('demo/fortune-tech.csv')
 df_heal = pd.read_csv('demo/fortune-heal.csv')
@@ -18,7 +20,28 @@ df_tech = df_tech.sample(n_tech)
 df_heal = df_heal.sample(n_heal)
 df_tech['Label'] = 'Technology'
 df_heal['Label'] = 'Healthcare'
-df_tech['Date'] = [np.random.choice(past_week) for _ in range(n_tech)]
-df_heal['Date'] = [np.random.choice(past_week) for _ in range(n_heal)]
+df_tech['Date'] = [today] * n_tech
+df_heal['Date'] = [today] * n_heal
 
-df = pd.concat([df_tech, df_heal])
+# Simulate unconfident predictions
+df_else = pd.DataFrame({
+    'Client': np.random.choice(['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'], size=n_else),
+    'Label': ['Other'] * n_else,
+    'Date': [today] * n_else
+})
+
+# Simulate queries over the last week
+df_done = pd.DataFrame({
+    'Client': ['Completed'] * n_done,
+    'Label': np.random.choice(['Technology', 'Healthcare'], size=n_done, p=[0.6, 0.4]),
+    'Date': np.random.choice(past_week, size=n_done)
+})
+
+df_chart = pd.concat([df_tech, df_heal, df_done])
+df = pd.concat([df_tech, df_heal, df_else])
+
+with open('demo/entities.txt', 'rt') as f:
+    entities = [x.strip() for x in f.readlines()]
+
+df_entities = pd.DataFrame([np.random.choice(entities, size=np.random.randint(2, 5)) for _ in range(len(df))])
+df = df.merge(df_entities, left_index=True, right_index=True)
