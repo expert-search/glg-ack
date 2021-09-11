7afcfe0460f62486cea56cbe87d809dedf024cfd
Andrew Minh Nguyen <amqdnguyen@gmail.com>
2021-09-11 05:13:32 +0000

    Near final demo iteration

--- src/deployment/app.py
+++ src/deployment/app.py
@@ -1,5 +1,6 @@
 import pickle
 from pathlib import Path
+from typing import Iterable
 
 import spacy
 import numpy as np
@@ -33,14 +34,16 @@ session_vars = {'bert_clf', 'bert_tkzr'}
 if not session_vars.intersection(set(st.session_state)):
     st.session_state.bert_tkzr = get_object('distilbert_tokenizer.pkl')
     st.session_state.bert_clf = get_bert_clf()
-    st.session_state.ner = spacy.load('en_core_web_md')  # spacy.load(model_dir/'kaggleTrainedNER10000')
+    st.session_state.ner_web = spacy.load('en_core_web_md')
+    st.session_state.ner_drug = spacy.load(model_dir/'drugNERmodel')
 
 def ss(key):
     return st.session_state[key]
 
 bert_tkzr = ss('bert_tkzr')
 bert_clf = ss('bert_clf')
-ner = ss('ner')
+ner_web = ss('ner_web')
+ner_drug = ss('ner_drug')
 
 ## Helpers
 
@@ -56,20 +59,25 @@ def predict(query, clf_type, probas=False):
 
     return pred if probas else np.argmax(pred)
 
-def apply_ner(query, ner):
-    pred = ner(query)
+def apply_ners(query, ners):
     ignore = ['CARDINAL', 'DATE', 'MONEY', 'ORDINAL', 'PERCENT', 'QUANTITY', 'TIME']
-    return [ent.text for ent in pred.ents if ent.label_ not in ignore]
+    if not isinstance(ners, Iterable):
+        ners = [ners]
+    preds = set()
+    for ner in ners:
+        pred = ner(query)
+        preds.update({e.text for e in pred.ents if e.label_ not in ignore})
+    return list(preds)
 
-def show_df(labels):
-    st.write(df[df['Label']].reset_index(drop=True))
+def show_df_by_tags(df, tags):
+    return st.dataframe(filter_df(df, tags)) if not 'Expert' in df.columns else st.dataframe(filter_df(df, tags), height=150, width=450)
 
 ## Apps
 if page == SIMPLE_PREDICTION:
     """
     # GLG-ACK
 
-    Our model automatically classifies client queries as being either healthcare-related or technology-related. You can test it out below.
+    We have two pipelines that operate on incoming text queries: a classification pipeline that predicts text as healthcare-related or technology-related, and a named entity recognition pipeline that tags entities in the text. You can test it out below.
     """
     query = st.text_area('Enter your query:')
     if len(query) == 0:
@@ -80,30 +88,23 @@ if page == SIMPLE_PREDICTION:
         pred = pd.DataFrame(pred, index=['probability'], columns=['Healthcare', 'Technology', 'Other'])
         st.write(pred.style.format(precision=4))
         st.write(f'Prediction: {pred.columns[np.argmax(pred)]}')
-        st.write(f'Entities: {apply_ner(query, ner)}')
+        st.write(f'Entities: {apply_ners(query, [ner_web, ner_drug])}')
 
 elif page == SHOWCASE:
-    # MORE FEATURES:
-    # - Provide DF with ['date_received', 'client', 'label', 'hover_for_full_query', 'NER hashtags'] -- clients can be random from Fortune-500
-    # - Hashtags show a DF with connected experts (random names)
-    # - Show graph of entity tags over time
-
     # Demo Setup
-    from demo import add_fake_metadata, get_unique_tags, get_filterable_df, filter_df, df_done
+    from demo import queries, add_fake_metadata, get_unique_tags, get_filterable_df, filter_df, add_random_expertise, df_experts, df_done
     if 'demo_df' not in st.session_state:
-        n = 10
         label_map = {0: 'Healthcare', 1: 'Technology', 2: 'Other'}
-        with open('demo/all_sentences_list.pkl', 'rb') as f:
-            queries = pickle.load(f)[:n]
         preds, tags = [], []
         pbar = st.progress(0)
         for i, item in enumerate(queries):
-            pbar.progress((i+1)/n)
+            pbar.progress((i+1)/len(queries))
             preds.append(label_map[predict(item, 'bert')])
-            tags.append(apply_ner(item, ss('ner')))
+            tags.append(apply_ners(item, [ner_web, ner_drug]))
         df = pd.DataFrame({'Label': preds, 'Hashtags': tags, 'Query': queries})
         st.session_state.demo_df = add_fake_metadata(df)
-    df = st.session_state.demo_df
+        st.session_state.df_experts = get_filterable_df(add_random_expertise(df, df_experts))
+    df, df_experts = ss('demo_df'), ss('df_experts')
     """
     # Welcome, GLG Agent
     ---
@@ -112,21 +113,22 @@ elif page == SHOWCASE:
     df_chart = df_chart[df_chart['Label'] != 'Other']
     c = alt.Chart(df_chart, title='Queries Over the Past Week').mark_line().encode(x=alt.X('monthdate(Date)', title='Day'), y=alt.Y('count(Client)', title='Num Queries'), color='Label').configure_legend(title=None, orient='bottom-left')
     st.altair_chart(c, use_container_width=True)
-    df_tech = df[df['Label'] == 'Technology']
-    df_heal = df[df['Label'] == 'Healthcare']
-    df_else = df[df['Label'] == 'Other']
+    df_tech = get_filterable_df(df[df['Label'] == 'Technology'])
+    df_heal = get_filterable_df(df[df['Label'] == 'Healthcare'])
+    df_else = get_filterable_df(df[df['Label'] == 'Other'])
+    help_text = 'In the box below, you can type or select to filter queries and experts by hashtag'
     f"## There are {len(df_tech)} technology queries available today:"
     with st.expander(''):
-        tags_tech = st.multiselect('', get_unique_tags(df_tech), help='In the box below, you can type or select to filter queries by hashtag')
-        df_tech = get_filterable_df(df_tech)
-        st.write(filter_df(df_tech, tags_tech))
+        tags_tech = st.multiselect('', get_unique_tags(df_tech), help=help_text)
+        show_df_by_tags(df_tech, tags_tech)
+        show_df_by_tags(df_experts, tags_tech)
     f"## There are {len(df_heal)} healthcare queries available today:"
     with st.expander(''):
-        tags_heal = st.multiselect('', get_unique_tags(df_heal), help='In the box below, you can type or select to filter queries by hashtag')
-        df_heal = get_filterable_df(df_heal)
-        st.write(filter_df(df_heal, tags_heal))
+        tags_heal = st.multiselect('', get_unique_tags(df_heal), help=help_text)
+        show_df_by_tags(df_heal, tags_heal)
+        show_df_by_tags(df_experts, tags_heal)
     f"## There are {len(df_else)} queries needing manual review today:"
     with st.expander(''):
-        tags_else = st.multiselect('', get_unique_tags(df_else), help='In the box below, you can type or select to filter queries by hashtag')
-        df_else = get_filterable_df(df_else)
-        st.write(filter_df(df_else, tags_else))
+        tags_else = st.multiselect('', get_unique_tags(df_else), help=help_text)
+        show_df_by_tags(df_else, tags_else)
+        show_df_by_tags(df_experts, tags_else)
